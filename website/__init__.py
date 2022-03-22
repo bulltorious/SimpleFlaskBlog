39feb5b3f7fd592ea87a60661ecf9cbb3a76d439
bullyellis <bullyellis@gmail.com>
2022-03-22 10:11:42 -0400

    simple fix for app.config db key

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQLALCHEMY_DATABASE_URI']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f''
 
     if os.environ['DATABASE_URL'] and os.environ['DATABASE_URL'].startswith("postgres://"):
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
